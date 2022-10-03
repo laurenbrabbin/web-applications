ae9d6321d478239b651b968146955ebbdc093019
Lauren Brabbin <laurenbrabbin@MASTUDENT-Lauren-Brabbin-C17G44KUQ6L4.local>
2022-10-03 17:07:02 +0100

    uploading 02_building_a_route challenges

--- hello_web_project/app.rb
+++ hello_web_project/app.rb
@@ -2,6 +2,17 @@ require 'sinatra/base'
 require 'sinatra/reloader'
 
 class Application < Sinatra::Base
+  get '/hello' do
+    name = params[:name]
+    return "Hello #{name}"
+  end
+  
+  post '/submit' do
+    name = params[:name]
+    message = params[:message]
+    return "Thanks #{name}, you send this message: #{message}"
+  end
+  
   # This allows the app code to refresh
   # without having to restart the server.
   configure :development do
